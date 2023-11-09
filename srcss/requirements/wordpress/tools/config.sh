cc1807026453e64e51ee18b4c90c6bca15d94a3c
mechane <mechane@student.1337.ma>
2023-11-09 12:49:28 +0100

    cx

--- srcss/requirements/wordpress/tools/config.sh
+++ srcss/requirements/wordpress/tools/config.sh
@@ -1,19 +1,19 @@
-#!bin/bash
-cd /var/www/wordpress
-wp core config	--dbhost=$DB_HOST \
-				--dbname=$DB_NAME \
-				--dbuser=$DB_USER \
-				--dbpass=$DB_PASSWORD \
-				--allow-root
+#!/bin/sh
+sleep 10
 
-wp core install --title=$ $TITLE\
-				--admin_user=$ADMIN_USER \
-				--admin_password=$ADMIN_PASSWORD \
-				--admin_email=$ADMIN_MAIL \
-				--url=$URL \
-				--allow-root
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
-wp user create $USER $USER_MAIL --role=author --user_pass=$USER_PASSWORD --allow-root
-cd -
+wp core download --path=/var/www/ --allow-root
 
-php-fpm7.3 -F
+cat /var/www/wp-config-sample.php >  /var/www/wp-config.php
+
+wp config set DB_NAME $DB_DATABASE --path=/var/www/ --allow-root
+wp config set DB_USER $DB_USER --path=/var/www/ --allow-root
+wp config set DB_PASSWORD $DB_PASSWORD --path=/var/www/ --allow-root
+wp config set DB_HOST $DB_HOST --path=/var/www/ --allow-root
+
+wp core install --path=/var/www/ --url=$URL --title=$TITLE --admin_user=$ADMIN_PASSWORD --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
+
+wp user create --path=/var/www/ $ADMIN_USER $ADMIN_EMAIL --user_pass=$ADMIN_PASSWORD --allow-root
+
+exec php-fpm7.3 -F
