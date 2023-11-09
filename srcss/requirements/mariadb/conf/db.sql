7cad04f0bcccbb73f4e7b3da879edf7c103bed77
mechane <mechane@student.1337.ma>
2023-11-09 13:46:55 +0100

    dg

--- srcss/requirements/mariadb/conf/db.sql
+++ srcss/requirements/mariadb/conf/db.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE my_database;
-CREATE USER mechane@localhost IDENTIFIED BY '123';
-GRANT ALL PRIVILEGES ON my_database.* TO mechane@'%';
+CREATE USER 'mechane'@'localhost' IDENTIFIED BY '123';
+GRANT ALL PRIVILEGES ON my_database.* TO 'mechane'@'%';
 FLUSH PRIVILEGES;
