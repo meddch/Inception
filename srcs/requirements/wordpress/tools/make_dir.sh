98a4e096ec54477c3c61aa5d26d30f23b7a47713
mechane <mechane@student.1337.ma>
2023-11-20 15:18:23 +0100

    modified:   srcs/requirements/wordpress/tools/make_dir.sh

--- srcs/requirements/wordpress/tools/make_dir.sh
+++ srcs/requirements/wordpress/tools/make_dir.sh
@@ -3,4 +3,5 @@ if [ ! -d "/home/mechane/Desktop/data" ]; then
         mkdir /home/mechane/Desktop/data/
         mkdir /home/mechane/Desktop/data/mariadb
         mkdir /home/mechane/Desktop/data/wordpress
-        mkdir /home/mechane/Desktop/data/portahome
+        mkdir /home/mechane/Desktop/data/portainer
+fi
